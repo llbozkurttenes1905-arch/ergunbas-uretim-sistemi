fb2f877029ba421898bf37b7164b0f09acab8f81
llbozkurttenes1905-arch <llbozkurttenes1905@gmail.com>
2026-09-23 14:29:10 +0300

    fix: reportlab TA_LEFT import eksikligi giderildi ve pdf indirme duzeltildi

--- app_backend.py
+++ app_backend.py
@@ -19,7 +19,7 @@ from reportlab.lib import colors
 from reportlab.lib.units import cm
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Table, TableStyle, PageBreak
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
-from reportlab.lib.enums import TA_CENTER, TA_RIGHT
+from reportlab.lib.enums import TA_CENTER, TA_RIGHT, TA_LEFT
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 
